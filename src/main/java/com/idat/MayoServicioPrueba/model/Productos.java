857c8a07b46ec0e6a8e20ceddd9ea0527a643c19
WilderEspinoza <wilder.espinozab@ciplima.org.pe>
2022-05-24 21:38:58 -0500

    Conexion con BD

--- src/main/java/com/idat/MayoServicioPrueba/model/Productos.java
+++ src/main/java/com/idat/MayoServicioPrueba/model/Productos.java
@@ -1,8 +1,17 @@
 package com.idat.MayoServicioPrueba.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Table(name = "Productos")
+@Entity
 public class Productos {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer idProducto;
 	private String nombreProducto;
 	private String descripcion;
