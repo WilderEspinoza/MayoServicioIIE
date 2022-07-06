713a001e51f23ec301372095fb6dccd2f84a9785
WilderEspinoza <wilder.espinozab@ciplima.org.pe>
2022-07-05 20:22:03 -0500

    Clase de OAUTH2

--- src/main/java/com/idat/MayoServicioPrueba/MayoServicioPruebaApplication.java
+++ src/main/java/com/idat/MayoServicioPrueba/MayoServicioPruebaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MayoServicioPruebaApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MayoServicioPruebaApplication.class, args);
 	}
 
--- src/main/java/com/idat/MayoServicioPrueba/security/ConfigSecurity.java
+++ src/main/java/com/idat/MayoServicioPrueba/security/ConfigSecurity.java
@@ -11,6 +11,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -19,9 +21,6 @@ public class ConfigSecurity extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private UserDetailService service;
-	
-	@Autowired
-	private TokenFilter filter;
 	
 	@Autowired
 	private EntryPoint entrypoint;
@@ -44,20 +43,25 @@ public class ConfigSecurity extends WebSecurityConfigurerAdapter {
 //			.and()
 //			.csrf().disable();
 		
-		http.authorizeRequests()
-			.antMatchers("/crearToken").permitAll()
-			.anyRequest()
-			.authenticated()
-			.and()
-			.exceptionHandling()
-			.authenticationEntryPoint(entrypoint)
-			.and()
-			.sessionManagement()
-			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			.and()
-			.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
-			.csrf().disable();
+//		http.authorizeRequests()
+//			.antMatchers("/crearToken").permitAll()
+//			.anyRequest()
+//			.authenticated()
+//			.and()
+//			.exceptionHandling()
+//			.authenticationEntryPoint(entrypoint)
+//			.and()
+//			.sessionManagement()
+//			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//			.and()
+//			.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+//			.csrf().disable();
 		
+	}
+	
+	@Bean
+	public TokenStore store() {
+		return new InMemoryTokenStore();
 	}
 	
 	@Bean
@@ -65,19 +69,12 @@ public class ConfigSecurity extends WebSecurityConfigurerAdapter {
 		return new BCryptPasswordEncoder();
 	}
 
+	@Bean
 	@Override
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 		// TODO Auto-generated method stub
 		return super.authenticationManagerBean();
 	}
-
-	@Override
-	protected AuthenticationManager authenticationManager() throws Exception {
-		// TODO Auto-generated method stub
-		return super.authenticationManager();
-	}
-	
-	
 
 
 }
